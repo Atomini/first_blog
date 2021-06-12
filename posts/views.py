e819a0edd2ed15aa2dd276c5d1c59655fec0eb5a
Atomini <62155406+Atomini@users.noreply.github.com>
2021-06-12 17:32:26 +0300

    added ordering by date

--- posts/views.py
+++ posts/views.py
@@ -8,6 +8,8 @@ from .models import BlogPosts
 class HomeView(ListView):
     model = BlogPosts
     template_name = "home.html"
+    paginate_by = 10
+    ordering = ["-published_time"]
 
 
 class PostDetailView(DetailView):
